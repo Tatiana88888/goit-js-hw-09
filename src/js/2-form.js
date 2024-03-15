92345f21ecea0c471f2e2a124c8cc3b0677a1409
Tatiana88888 <147710787+Tatiana88888@users.noreply.github.com>
2024-03-15 12:22:57 +0200

    add correction3

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,7 +5,7 @@ const textareaEl = formEl.querySelector('textarea');
 formEl.addEventListener('input', handleInput);
 
 function handleInput(event) {
-  const message = textareaEl.value.trim();
+  const message = formEl.elements.message.value.trim();
   const email = formEl.elements.email.value.trim();
   const data = JSON.stringify({ message, email });
   localStorage.setItem(STORAGE_KEY, data);
@@ -14,9 +14,9 @@ function handleInput(event) {
 const dataJsn = localStorage.getItem(STORAGE_KEY) ?? '';
 try {
   const dataStorage = JSON.parse(dataJsn);
-  if (dataStorage && dataStorage.message && dataStorage.email)
+  if (dataStorage && dataStorage.message || dataStorage.email)
   {
-    textareaEl.value = dataStorage.message;
+  formEl.elements.message.value = dataStorage.message;
   formEl.elements.email.value = dataStorage.email;}
 } catch {
   console.log('no data saved');
@@ -25,14 +25,14 @@ try {
 formEl.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
-  const messageTrimmed = textareaEl.value.trim();
+  const messageTrimmed = formEl.elements.message.value.trim();
   const emailTrimmed = formEl.elements.email.value.trim();
   if (messageTrimmed === '' || emailTrimmed === '') {
     return alert('Заповніть усі поля форми!');
   } else {
     const info = {
       email: formEl.elements.email.value,
-      message: textareaEl.value,
+      message: formEl.elements.message.value,
     };
     console.log(info);
     event.target.reset();
